994f2e2d3801ab469a16c1bac13412e8220b303e
gyuseok <yigyuseok@gmail.com>
2023-08-03 01:45:28 +0900

    부동산 APP

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(a,i) in menus" :key="i">{{ a }}</a>
+  </div>
+  <div v-for="(a,i) in productList" :key="i">
+    <h4>{{a.name}}</h4>
+    <p>{{ a.price }} 만원</p>
+    <button v-on:click="increaseReport(i)">허위매물신고</button> <span>신고수 : {{ a.report }}</span>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      price1: 70,
+      price2: 80,
+      style1: 'color : blue',
+      productList:[
+        {
+          name :'역삼동 원룸',
+          price : '80',
+          report: 0
+        },
+        {
+          name :'청덕동 원룸',
+          price : '50',
+          report: 0
+        },
+        {
+          name :'천호동 원룸',
+          price : '70',
+          report: 0
+        }
+      ],
+      menus : ['Home','Shop','About']
+    }
+  },
+  methods:{
+    increaseReport(i){
+      this.productList[i].report++;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
+
 
 <style>
 #app {
